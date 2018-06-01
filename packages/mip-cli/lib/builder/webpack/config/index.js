6c41a3dadc891af9df04c17a175bc647a16a0619
chenqiushi <tracy909@126.com>
2018-06-01 16:27:44 +0800

    eslint format

--- packages/mip-cli/lib/builder/webpack/config/index.js
+++ packages/mip-cli/lib/builder/webpack/config/index.js
@@ -3,82 +3,81 @@
  * @author clark-t (clarktanglei@163.com)
  */
 
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 // const sandboxLoader = require('./sandbox-loader');
-const styleLoaders = require('./style-loaders');
-const CustomElementPlugin = require('./custom-element-plugin');
+const styleLoaders = require('./style-loaders')
+const CustomElementPlugin = require('./custom-element-plugin')
 /* eslint-disable */
 const {resolveModule} = require('../../../utils/helper');
 /* eslint-enable */
-const {babelLoader, babelExternals} = require('./babel');
-const path = require('path');
+const {babelLoader, babelExternals} = require('./babel')
+const path = require('path')
 
 module.exports = function (options) {
-    return {
-        entry: options.entry,
-        output: {
-            path: options.outputPath,
-            filename: '[name]/[name].js',
-            publicPath: options.asset.replace(/\/$/, '') + '/'
-        },
-        mode: options.mode,
-        context: options.context,
-        devtool: options.mode === 'development' ? 'inline-source-map' : false,
-        module: {
-            rules: [
-                {
-                    test: /\.vue$/,
-                    use: [
-                        {
-                            loader: resolveModule('vue-loader'),
-                            options: {
-                                productionMode: options.mode === 'production'
-                            }
-                        }
-                    ]
-                },
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    use: [
-                        // sandboxLoader,
-                        babelLoader
-                    ]
-                },
-                {
-                    test: /\.(png|jpe?g|gif)$/,
-                    use: [{
-                        loader: resolveModule('url-loader'),
-                        options: {
-                            limit: 1000,
-                            name: 'img/[name]-[hash].[ext]'
-                        }
-                    }]
-                },
-                {
-                    test: /\.(otf|ttf|eot|svg|woff2?)(\?[a-z0-9=&.]+)?$/,
-                    use: [{
-                        loader: resolveModule('url-loader'),
-                        options: {
-                            limit: 1000,
-                            name: 'font/[name]-[hash].[ext]'
-                        }
-                    }]
-                },
-                ...styleLoaders
-            ]
-        },
-        externals: babelExternals,
-        resolve: {
-            extensions: ['.js', '.json', '.vue'],
-            alias: {
-                '@': path.resolve(options.context)
+  return {
+    entry: options.entry,
+    output: {
+      path: options.outputPath,
+      filename: '[name]/[name].js',
+      publicPath: options.asset.replace(/\/$/, '') + '/'
+    },
+    mode: options.mode,
+    context: options.context,
+    devtool: options.mode === 'development' ? 'inline-source-map' : false,
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          use: [
+            {
+              loader: resolveModule('vue-loader'),
+              options: {
+                productionMode: options.mode === 'production'
+              }
             }
+          ]
         },
-        plugins: [
-            new VueLoaderPlugin(),
-            new CustomElementPlugin()
-        ]
-    };
-};
-
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [
+            // sandboxLoader,
+            babelLoader
+          ]
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/,
+          use: [{
+            loader: resolveModule('url-loader'),
+            options: {
+              limit: 1000,
+              name: 'img/[name]-[hash].[ext]'
+            }
+          }]
+        },
+        {
+          test: /\.(otf|ttf|eot|svg|woff2?)(\?[a-z0-9=&.]+)?$/,
+          use: [{
+            loader: resolveModule('url-loader'),
+            options: {
+              limit: 1000,
+              name: 'font/[name]-[hash].[ext]'
+            }
+          }]
+        },
+        ...styleLoaders
+      ]
+    },
+    externals: babelExternals,
+    resolve: {
+      extensions: ['.js', '.json', '.vue'],
+      alias: {
+        '@': path.resolve(options.context)
+      }
+    },
+    plugins: [
+      new VueLoaderPlugin(),
+      new CustomElementPlugin()
+    ]
+  }
+}
--- packages/mip-cli/lib/generate.js
+++ packages/mip-cli/lib/generate.js
@@ -3,15 +3,15 @@
  * @author tracy(qiushidev@gmail.com)
  */
 
-const async = require('async');
-const path = require('path');
-const ms = require('metalsmith');
-const inquirer = require('inquirer');
-const render = require('./utils/render').render;
-const getMeta = require('./utils/meta');
-const home = require('user-home');
+const async = require('async')
+const path = require('path')
+const ms = require('metalsmith')
+const inquirer = require('inquirer')
+const render = require('./utils/render').render
+const getMeta = require('./utils/meta')
+const home = require('user-home')
 // 本地临时目录
-const tmp = path.join(home, '.mip-template');
+const tmp = path.join(home, '.mip-template')
 
 /**
  * 生成项目 or 组件结构
@@ -20,85 +20,80 @@ const tmp = path.join(home, '.mip-template');
  * @param {string} compName 组件名称，仅用于只渲染组件的情况，渲染整个项目不传即可
  * @param {Function} done 回调函数
  */
-module.exports = function generate(dir, compName, done) {
-    const metalsmith = ms(path.join(tmp, dir));
-    const templatePrompts = getMeta(tmp).prompts;
+module.exports = function generate (dir, compName, done) {
+  const metalsmith = ms(path.join(tmp, dir))
+  const templatePrompts = getMeta(tmp).prompts
 
-    metalsmith
-        .use(ask(templatePrompts))
-        .use(renderTemplateFiles())
-        .clean(false)
-        .source('.') // start from template root instead of `./src` which is Metalsmith's default for `source`
-        .build(err => {
-            done(err);
-        });
+  metalsmith
+    .use(ask(templatePrompts))
+    .use(renderTemplateFiles())
+    .clean(false)
+    .source('.') // start from template root instead of `./src` which is Metalsmith's default for `source`
+    .build(err => {
+      done(err)
+    })
 
-    function ask(templatePrompts) {
-        return (files, metalsmith, done) => {
-            // 替换 components 目录组件名称
-            metalsmith.metadata().compName = compName || 'mip-example';
+  function ask (templatePrompts) {
+    return (files, metalsmith, done) => {
+      // 替换 components 目录组件名称
+      metalsmith.metadata().compName = compName || 'mip-example'
 
-            // 只渲染组件部分时(mip2 add)，不需要走 ask 流程，且使用 component name 作为 dest 路径
-            if (compName) {
-                metalsmith.destination(path.resolve('./components', compName));
-                return done();
-            }
+      // 只渲染组件部分时(mip2 add)，不需要走 ask 流程，且使用 component name 作为 dest 路径
+      if (compName) {
+        metalsmith.destination(path.resolve('./components', compName))
+        return done()
+      }
 
-            async.eachSeries(Object.keys(templatePrompts), run, () => {
-                // After all inquirer finished, set destination directory with input project name
-                metalsmith.destination(path.resolve('./', metalsmith.metadata().name));
-                done();
-            });
+      async.eachSeries(Object.keys(templatePrompts), run, () => {
+        // After all inquirer finished, set destination directory with input project name
+        metalsmith.destination(path.resolve('./', metalsmith.metadata().name))
+        done()
+      })
 
-            function run(key, done) {
-                let prompt = templatePrompts[key];
+      function run (key, done) {
+        let prompt = templatePrompts[key]
 
-                inquirer.prompt([{
-                    'type': prompt.type,
-                    'name': key,
-                    'message': prompt.message || prompt.label || key,
-                    'default': prompt.default,
-                    'validate': prompt.validate || (() => true)
-                }]).then(answers => {
-                    if (typeof answers[key] === 'string') {
-                        metalsmith.metadata()[key] = answers[key].replace(/"/g, '\\"');
-                    }
-                    else {
-                        metalsmith.metadata()[key] = answers[key];
-                    }
-                    done();
-                }).catch(done);
-            }
-        };
+        inquirer.prompt([{
+          'type': prompt.type,
+          'name': key,
+          'message': prompt.message || prompt.label || key,
+          'default': prompt.default,
+          'validate': prompt.validate || (() => true)
+        }]).then(answers => {
+          if (typeof answers[key] === 'string') {
+            metalsmith.metadata()[key] = answers[key].replace(/"/g, '\\"')
+          } else {
+            metalsmith.metadata()[key] = answers[key]
+          }
+          done()
+        }).catch(done)
+      }
     }
+  }
 
-    function renderTemplateFiles() {
-        return (files, metalsmith, done) => {
-            let keys = Object.keys(files);
-            async.each(keys, run, done);
+  function renderTemplateFiles () {
+    return (files, metalsmith, done) => {
+      let keys = Object.keys(files)
+      async.each(keys, run, done)
 
-            function run(file, done) {
-                let str = files[file].contents.toString();
+      function run (file, done) {
+        let str = files[file].contents.toString()
 
-                // do not attempt to render files that do not have mustaches
-                if (!/{{([^{}]+)}}/g.test(str)) {
-                    return done();
-                }
+        // do not attempt to render files that do not have mustaches
+        if (!/{{([^{}]+)}}/g.test(str)) {
+          return done()
+        }
 
-                let res;
-                try {
-                    res = render(str, metalsmith.metadata());
-                }
-                catch (err) {
-                    return done(err);
-                }
+        let res
+        try {
+          res = render(str, metalsmith.metadata())
+        } catch (err) {
+          return done(err)
+        }
 
-                files[file].contents = new Buffer(res);
-                done();
-            }
-        };
+        files[file].contents = Buffer.from(res)
+        done()
+      }
     }
-
-};
-
-
+  }
+}
