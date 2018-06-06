8316b4911e4166aa03b6e7cd334491b4c12868e2
clarkt <clarktanglei@163.com>
2018-06-07 03:08:46 +0800

    增加 is 的测试用例

--- packages/mip-sandbox/test/utils.spec.js
+++ packages/mip-sandbox/test/utils.spec.js
@@ -8,8 +8,9 @@ var chai = require('chai')
 var expect = chai.expect
 
 var defUtils = require('../lib/utils/def')
+var is = require('../lib/utils/is')
 
-describe('util/def', function () {
+describe('utils/def', function () {
   describe('#defs', function () {
     var defs = defUtils.defs
 
@@ -29,17 +30,17 @@ describe('util/def', function () {
       defs(obj, ['setTimeout', 'isNaN'], {host: global})
 
       expect(obj.setTimeout).to.not.be.equal(setTimeout)
-      expect(obj.isNaN(NaN)).to.be.ok
+      expect(obj.isNaN(NaN)).to.be.equal(true)
     })
 
     it('define property', function () {
       var obj = {}
       // 别个 什么 location 在 当前测试环境下都没有诶
       defs(obj, ['undefined'], {host: global})
-      expect(obj.undefined).to.be.undefined
+      expect(obj.undefined).to.be.equal(undefined)
       // writable = false
       obj.undefined = 1
-      expect(obj.undefined).to.be.undefined
+      expect(obj.undefined).to.be.equal(undefined)
     })
   })
 
@@ -51,5 +52,86 @@ describe('util/def', function () {
       def(obj, 'sandbox', sandbox)
       expect(obj.sandbox).to.be.equal(sandbox)
     })
+  })
+})
+
+describe('utils/is', function () {
+  it('#string type', function () {
+    expect(is({type: 'Identifier'}, 'Identifier')).to.be.equal(true)
+  })
+
+  it('#regexp type', function () {
+    expect(is({type: 'ArrowFunctionExpression'}, /Function/)).to.be.equal(true)
+    expect(is({type: 'FunctionExpression'}, /Function/)).to.be.equal(true)
+    expect(is({type: 'FunctionDeclaration'}, /Function/)).to.be.equal(true)
+  })
+
+  it('#params', function () {
+    expect(is({type: 'Identifier'}, 'Identifier', {name: 'a'})).to.be.equal(false)
+    expect(is({type: 'Identifier', name: 'a'}, 'Identifier', {name: 'a'})).to.be.equal(true)
+  })
+
+  it('#array params', function () {
+    var prop1 = {
+      type: 'Property'
+    }
+    var prop2 = {
+      type: 'Property'
+    }
+
+    expect(
+      is(
+        {
+          type: 'ObjectExpression',
+          properties: [prop1, prop2]
+        },
+        'ObjectExpression',
+        {properties: [prop1, prop2]}
+      )
+    ).to.be.equal(true)
+
+    expect(
+      is(
+        {
+          type: 'ObjectExpression',
+          properties: [prop1, prop2]
+        },
+        'ObjectExpression',
+        {properties: [prop2]}
+      )
+    ).to.be.equal(true)
+
+    expect(
+      is(
+        {
+          type: 'ObjectExpression',
+          properties: [prop1, prop2]
+        },
+        'ObjectExpression',
+        {properties: []}
+      )
+    ).to.be.equal(true)
+
+    expect(
+      is(
+        {
+          type: 'ObjectExpression',
+          properties: [prop1]
+        },
+        'ObjectExpression',
+        {properties: [prop1, prop2]}
+      )
+    ).to.be.equal(false)
+
+    expect(
+      is(
+        {
+          type: 'ObjectExpression',
+          properties: [prop1]
+        },
+        'Identifier',
+        {properties: [prop2]}
+      )
+    ).to.be.equal(false)
   })
 })
