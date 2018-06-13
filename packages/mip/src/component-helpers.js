38419c69d96920bd11b6fa31d7e3a839e31a6cf1
clark-t <clarktanglei@163.com>
2018-06-13 19:54:06 +0800

    增加个别 babel polyfill 并挂载到 window

--- packages/mip/src/component-helpers.js
+++ packages/mip/src/component-helpers.js
@@ -11,6 +11,26 @@ const componentNormalizer = require('vue-loader/lib/runtime/componentNormalizer'
 const addStylesClient = require('vue-style-loader/lib/addStylesClient')
 const listToStyles = require('vue-style-loader/lib/listToStyles')
 
+const promise = require('babel-runtime/core-js/promise').default
+const symbol = require('babel-runtime/core-js/symbol').default
+const arrayFrom = require('babel-runtime/core-js/array/from').default
+const objectAssign = require('babel-runtime/core-js/object/assign').default
+
+function mount (obj, name, host = window) {
+  if (typeof host[name] === 'undefined') {
+    host[name] = obj
+    // Object.defineProperty(host, name, {
+    //   value: obj,
+    //   enumerable: true
+    // })
+  }
+}
+
+mount(promise, 'Promise')
+mount(symbol, 'Symbol')
+mount(arrayFrom, 'from', Array)
+mount(objectAssign, 'assign', Object)
+
 let helpers = {}
 
 requires.keys().forEach(filename => {
@@ -23,6 +43,5 @@ helpers['css-loader/lib/css-base'] = cssBase
 helpers['vue-loader/lib/runtime/componentNormalizer'] = componentNormalizer
 helpers['vue-style-loader/lib/addStylesClient'] = addStylesClient
 helpers['vue-style-loader/lib/listToStyles'] = listToStyles
-
 
 export default helpers
