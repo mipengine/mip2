e9890ee491ea989de018cb01d9442d623b9af232
chenqiushi <tracy909@126.com>
2018-06-20 18:23:38 +0800

    feat: cli support plugin

---
+++ packages/mip-cli/lib/addPlugin.js
@@ -0,0 +1,82 @@
+/**
+ * @file addPlugin 添加自定义扩展组件
+ * @author tracy(qiushidev@gmail.com)
+ */
+
+const path = require('path')
+const fs = require('fs')
+const {
+  resolvePluginName,
+  isInstalled,
+  installPackage,
+  resolve,
+  resolveCommand
+} = require('./utils/plugin')
+const cli = require('./cli')
+
+module.exports = async function addPlugin ({pluginName, options = {}, context = process.cwd()}) {
+  if (!pluginName) {
+    return cli.error(`pluginName not found`)
+  }
+
+  const packageName = resolvePluginName(pluginName)
+
+  // install if necessary
+  if (!isInstalled(packageName)) {
+    cli.info(`Ready to install ${cli.chalk.cyan(packageName)} automatically...`)
+    await installPackage(path.resolve(__dirname, '../../..'), 'npm', packageName)
+    cli.info(`${cli.chalk.cyan(packageName)} successfully installed`)
+  }
+
+  // invoke command
+
+  /**
+   * resolve cmd module path to execuate
+   *
+   * @param {String} cliDir cli directory
+   * @param {String} args input arguements
+   * @return {String} command module path which will be execuate
+   */
+  const resolveCmdPath = (cliDir, args) => {
+    const mainCommand = resolveCommand(args[0])
+    const subCommand = args[1]
+
+    // scan cli dir to check if args of main command is a subcommand
+    const pkgSubCommandDir = path.join(cliDir, mainCommand)
+
+    let pkgSubCommands = []
+    if (fs.existsSync(pkgSubCommandDir)) {
+      pkgSubCommands = fs.readdirSync(path.join(cliDir, mainCommand)).map(file => path.basename(file, '.js'))
+    }
+
+    // match cli file with command name
+    if (pkgSubCommands.indexOf(subCommand) !== -1) {
+      return path.join(cliDir, mainCommand, subCommand)
+    }
+
+    return path.join(cliDir, mainCommand)
+  }
+
+  // user custom plugin dir: mip-cli-plugin-xxx/cli
+  const cliPath = path.join(resolve(packageName), 'cli')
+
+  let commandModule
+  try {
+    const cmdPath = resolveCmdPath(cliPath, options.args)
+    commandModule = require(cmdPath)
+  } catch (e) {
+    cli.error(`Cannot find command module`)
+    return
+  }
+
+  // setup cli configuration
+  cli.setup(commandModule.cli.config)
+
+  // help information
+  if (options.rawArgs.indexOf('--help') !== -1 || options.rawArgs.indexOf('-h') !== -1) {
+    return cli.program.help()
+  }
+
+  // invoke main
+  commandModule.cli.main(options.args, cli.program)
+}
---
+++ packages/mip-cli/lib/utils/plugin.js
@@ -0,0 +1,106 @@
+/**
+ * @file plugins 相关的方法
+ * @author tracy(qiushidev@gmail.com)
+ */
+
+const path = require('path')
+const fs = require('fs')
+const execa = require('execa')
+const chalk = require('chalk')
+
+// plugin name rules
+// common：mip-cli-plugin-xxx
+// under specific scope：@somescope/mip-cli-plugin-xxx
+const pluginREG = /^(mip-|@[\w-]+\/mip-)cli-plugin-/
+
+exports.resolvePluginName = name => {
+  if (pluginREG.test(name)) {
+    return name
+  }
+
+  return `mip-cli-plugin-${name}`
+}
+
+exports.installPackage = async (targetDir, command, packageName) => {
+  const args = ['install', '--loglevel', 'error']
+  args.push(packageName)
+  await exports.executeCommand(command, args, targetDir)
+}
+
+exports.isInstalled = pkg => {
+  // const packageDir = path.resolve(__dirname, '../../..', pkg)
+  const packageDir = path.resolve(__dirname, '../../../../node_modules', pkg)
+  return fs.existsSync(packageDir)
+}
+
+exports.executeCommand = (command, args, targetDir) => {
+  return new Promise((resolve, reject) => {
+    const child = execa(command, args, {
+      cwd: targetDir,
+      stdio: 'inherit'
+    })
+
+    child.on('close', code => {
+      if (code !== 0) {
+        reject(new Error(`command failed: ${command} ${args.join(' ')}`))
+        return
+      }
+      resolve()
+    })
+  })
+}
+
+exports.resolve = pkg => {
+  function fix (dir) {
+    let idx = dir.lastIndexOf(pkg)
+    if (idx === -1) {
+      throw new Error('Invalid package name')
+    }
+    dir = dir.substr(0, idx + pkg.length)
+
+    return dir
+  }
+
+  return fix(require.resolve(pkg))
+}
+
+exports.resolveCommand = pkg => {
+  // if args[0] is full package name, covert to executable main command
+  // `mip2 mip-cli-pulgin-test` => `mip2 test`
+  return pkg.replace(pluginREG, '')
+}
+
+exports.showPluginCmdHelpInfo = () => {
+  let pluginsPkgs = exports.getPluginPackages()
+
+  if (!pluginsPkgs.length) {
+    return
+  }
+
+  console.log()
+  console.log('  User Plugin Commands:')
+  console.log()
+
+  pluginsPkgs.forEach(pkg => {
+    let cmd
+    let description
+    try {
+      cmd = exports.resolveCommand(pkg)
+      const cmdModule = require(path.join(exports.resolve(pkg), 'cli', cmd))
+      description = cmdModule.cli.config.description
+    } catch (e) {}
+
+    if (cmd && description) {
+      console.log(`    ${cmd}       ${chalk.green(description)}`)
+    }
+
+    console.log()
+  })
+}
+
+exports.getPluginPackages = () => {
+  // let installDir = path.join(__dirname, '../../..')
+  let installDir = path.join(__dirname, '../../../../node_modules')
+  let pkgs = fs.readdirSync(installDir)
+  return pkgs.filter(item => pluginREG.test(item))
+}
