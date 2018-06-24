2fe5f8f2587ad00545f576920e0059fc859f1c59
clark-t <clarktanglei@163.com>
2018-06-25 03:50:40 +0800

    增加功能

--- packages/mip-cli/lib/builder/webpack/config/custom-element-plugin.js
+++ packages/mip-cli/lib/builder/webpack/config/custom-element-plugin.js
@@ -6,12 +6,26 @@
 const path = require('path')
 const WrapperPlugin = require('wrapper-webpack-plugin')
 
-const customElementHeader = `(function () {
+function isComponentFile (filename) {
+  return /(mip-[\w-]+)\/\1\.js$/.test(filename)
+}
+
+function customElementHeader (filename) {
+  if (!isComponentFile(filename)) {
+    return ''
+  }
+
+  return `(function () {
     var __mip_component__ = `
+}
 
 function customElementFooter (filename) {
-  let ext = path.extname(filename)
-  let basename = path.basename(filename, ext)
+  if (!isComponentFile(filename)) {
+    return ''
+  }
+
+  let basename = path.basename(filename, path.extname(filename))
+  // 这个插件里的 filename 是 output 的 filename 所以没办法通过判断后缀的方式去调用 registerCustomElement 或者 registerVueCustomElement
   return `
         __mip_component__ = __mip_component__.default || __mip_component__;
         MIP[typeof __mip_component__ === 'function' ? 'registerCustomElement' : 'registerVueCustomElement'](
