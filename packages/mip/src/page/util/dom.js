f66e0cb5880d996ae12c39b86a8126aa7c5b03a6
Wangyisheng <wangyisheng@baidu.com>
2018-06-27 18:53:50 +0800

    去掉iframe的高度

--- packages/mip/src/page/util/dom.js
+++ packages/mip/src/page/util/dom.js
@@ -4,7 +4,7 @@
  */
 
 import css from '../../util/dom/css'
-import viewport from '../../viewport'
+// import viewport from '../../viewport'
 
 import {MIP_IFRAME_CONTAINER} from '../const/index'
 import {raf, transitionEndEvent, animationEndEvent} from './feature-detect'
@@ -35,7 +35,7 @@ export function createIFrame (fullpath, pageId, {onLoad, onError} = {}) {
    * Fix an iOS iframe width bug, see examples/mip1/test.html
    * https://stackoverflow.com/questions/23083462/how-to-get-an-iframe-to-be-responsive-in-ios-safari
    */
-  container.style.height = `${viewport.getHeight()}px`
+  // container.style.height = `${viewport.getHeight()}px`
   container.setAttribute('width', '100%')
   container.setAttribute('scrolling', 'no')
 
