11a5ba8cafdd54bc6da1d6936308f4b9ade1d60a
Ricardo-Li <liwenqianmail@gmail.com>
2018-07-19 10:48:59 +0800

    test: mip-component-validator

---
+++ packages/mip-component-validator/test/mip-cases/mip-component-style-no-fixed.vue
@@ -0,0 +1,10 @@
+<template>
+    <h3> component styles not scoped </h3>
+</template>
+
+<style scoped>
+.mip-test {
+    position: fixed;
+    color: red;
+}
+</style>
---
+++ packages/mip-component-validator/test/mip-cases/mip-disallowed-global-var.vue
@@ -0,0 +1,7 @@
+<template>
+    <h3> disallowed global var</h3>
+</template>
+
+<script>
+BAIDU.sayHello()
+</script>
