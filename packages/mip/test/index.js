c072796c65de17088d8c7506f9ffbfc627b56351
Wangyisheng <wangyisheng@baidu.com>
2018-07-24 10:11:00 +0800

    修正一处测试代码

--- packages/mip/test/index.js
+++ packages/mip/test/index.js
@@ -1,4 +1,3 @@
 // require all test files
 const testsContext = require.context('./', true, /specs\/.+\.spec$/)
-// const testsContext = require.context('./', true, /specs\/components\/mip-shell\.spec$/)
 testsContext.keys().forEach(testsContext)
