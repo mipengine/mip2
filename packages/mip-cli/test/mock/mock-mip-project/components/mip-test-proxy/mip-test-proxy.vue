a75fccba8bf8ab822f5bee95603382583b3eaf7b
clark-t <clarktanglei@163.com>
2018-08-01 11:23:24 +0800

    add test case

---
+++ packages/mip-cli/test/mock/mock-mip-project/components/mip-test-proxy/mip-test-proxy.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="wrapper">
+    <h1>MIP 2.0 component example</h1>
+    <h3>This is my first custom component !</h3>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  margin: 0 auto;
+  text-align: center;
+}
+</style>
+
+<script>
+export default {
+  mounted () {
+    console.log('str with prefix http://www.baidu.com/')
+    console.log('http://www.baidu.com/ str with ext')
+    console.log('http://www.baidu.com')
+  }
+}
+</script>
