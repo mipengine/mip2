bb2ab8b17fe8c641d2fa9d188fd5bdb295d43393
Wangyisheng <wangyisheng@baidu.com>
2018-08-03 10:44:44 +0800

    hack中需要还原mask

--- packages/mip/src/components/mip-shell/index.js
+++ packages/mip/src/components/mip-shell/index.js
@@ -580,7 +580,7 @@ class MipShell extends CustomElement {
           css(mask, {
             display: 'none',
             opacity: '',
-            pointerEvents: 'none'
+            pointerEvents: ''
           })
         }, 20)
       }
