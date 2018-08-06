12642e7f3415f7d494516b463122e667628480cc
Wangyisheng <wangyisheng@baidu.com>
2018-08-06 11:42:30 +0800

    改叫cacheFirst

--- packages/mip/src/viewer.js
+++ packages/mip/src/viewer.js
@@ -186,9 +186,9 @@ let viewer = {
    * @param {boolean} options.isMipLink Whether targetUrl is a MIP page. If not, use `top.location.href`. Defaults to `true`
    * @param {boolean} options.replace If true, use `history.replace` instead of `history.push`. Defaults to `false`
    * @param {Object} options.state Target page info
-   * @param {Object} options.fromCache If true, use cached iframe when available
+   * @param {Object} options.cacheFirst If true, use cached iframe when available
    */
-  open (to, {isMipLink = true, replace = false, state, fromCache} = {}) {
+  open (to, {isMipLink = true, replace = false, state, cacheFirst} = {}) {
     if (!state) {
       state = {click: undefined, title: undefined, defaultTitle: undefined}
     }
@@ -244,7 +244,7 @@ let viewer = {
       // Reload page even if it's already existed
       targetRoute.meta = {
         reload: true,
-        fromCache,
+        cacheFirst,
         header: {
           title: pushMessage.state.title,
           defaultTitle: pushMessage.state.defaultTitle
@@ -345,7 +345,7 @@ let viewer = {
       let to = $a.href
       let isMipLink = $a.hasAttribute('mip-link') || $a.getAttribute('data-type') === 'mip'
       let replace = $a.hasAttribute('replace')
-      let fromCache = $a.hasAttribute('from-cache')
+      let cacheFirst = $a.hasAttribute('cache-first')
       let state = self._getMipLinkData.call($a)
 
       /**
@@ -360,7 +360,7 @@ let viewer = {
         return
       }
 
-      self.open(to, {isMipLink, replace, state, fromCache})
+      self.open(to, {isMipLink, replace, state, cacheFirst})
 
       event.preventDefault()
     }, false)
