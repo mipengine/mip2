9baa5a1bbbbaacb8670f202871c64ac9f1291c29
yenshih <yenshih95@gmail.com>
2018-11-27 17:32:08 +0800

    fix: Change bundle format to iife

--- packages/mip/build/config.js
+++ packages/mip/build/config.js
@@ -26,14 +26,14 @@ const builds = {
   'mip-prod': {
     entry: resolve('mip'),
     dest: resolve('dist/mip.js'),
-    format: 'umd',
+    format: 'iife',
     env: 'production',
     banner: 'window._mipStartTiming=Date.now();'
   },
   'mip-vue': {
     entry: resolve('mip-vue'),
     dest: resolve('dist/mip-vue.js'),
-    format: 'umd',
+    format: 'iife',
     env: 'production'
   }
 }
