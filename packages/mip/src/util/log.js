a28997f63f40290338b94eaa9c373e6e99fb9ca6
sekiyika <pengxing5501854@gmail.com>
2019-01-15 10:54:58 +0800

    升级 log 支持多个参数

--- packages/mip/src/util/log.js
+++ packages/mip/src/util/log.js
@@ -4,8 +4,8 @@ const LIST = ['info', 'log', 'warn', 'error']
 class Log {
   constructor (name) {
     LIST.forEach(item => {
-      this[item] = log => {
-        console[item](`[${name}] ${log}`)
+      this[item] = (...args) => {
+        console[item](`[${name}]`, ...args)
       }
     })
   }
