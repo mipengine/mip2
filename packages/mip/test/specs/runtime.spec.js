ef80d3adcc4ba0de99fe0749238cd7badfdfd464
Shi Yan <yenshih95@gmail.com>
2019-01-24 15:56:07 +0800

    feat: 移除 service 对 window 的依赖 (#493)

--- packages/mip/test/specs/runtime.spec.js
+++ packages/mip/test/specs/runtime.spec.js
@@ -15,7 +15,6 @@ describe('runtime', () => {
   it('should register services', () => {
     const services = window.services
     expect(services.extensions).to.be.an('object')
-    expect(services.mipdoc).to.be.an('object')
     expect(services.timer).to.be.an('object')
   })
 })
--- packages/mip/test/specs/services/services.spec.js
+++ packages/mip/test/specs/services/services.spec.js
@@ -24,18 +24,18 @@ describe('services', () => {
     })
 
     it('should make per window singletons', () => {
-      Services.registerService(window, 'a', Constructor)
-      const a1 = Services.getService(window, 'a')
-      Services.registerService(window, 'a', Constructor)
-      const a2 = Services.getService(window, 'a')
+      Services.registerService('a', Constructor)
+      const a1 = Services.getService('a')
+      Services.registerService('a', Constructor)
+      const a2 = Services.getService('a')
       expect(a1).to.equal(a2)
       expect(Constructor).to.be.calledOnce
       expect(Constructor.args[0][0]).to.equal(window)
 
-      Services.registerService(window, 'b', Constructor)
-      const b1 = Services.getService(window, 'b')
-      Services.registerService(window, 'b', Constructor)
-      const b2 = Services.getService(window, 'b')
+      Services.registerService('b', Constructor)
+      const b1 = Services.getService('b')
+      Services.registerService('b', Constructor)
+      const b2 = Services.getService('b')
       expect(b1).to.equal(b2)
       expect(b1).to.not.equal(a1)
       expect(Constructor).to.have.callCount(2)
@@ -43,22 +43,22 @@ describe('services', () => {
     })
 
     it('should not instantiate service when registered', () => {
-      Services.registerService(window, 'a', Constructor)
+      Services.registerService('a', Constructor)
       expect(Constructor).to.not.be.called
-      Services.getService(window, 'a')
+      Services.getService('a')
       expect(Constructor).to.be.calledOnce
     })
 
     it('should only instantiate the service once', () => {
-      Services.registerService(window, 'a', Constructor)
-      Services.getService(window, 'a')
+      Services.registerService('a', Constructor)
+      Services.getService('a')
       expect(Constructor).to.be.calledOnce
-      Services.getService(window, 'a')
+      Services.getService('a')
       expect(Constructor).to.be.calledOnce
     })
 
     it('should instantiate service immediately', () => {
-      Services.registerService(window, 'a', Constructor, true)
+      Services.registerService('a', Constructor, true)
       const a = window.services.a
       expect(a.instance).instanceOf(Constructor)
       expect(a.context).to.be.null
@@ -66,24 +66,24 @@ describe('services', () => {
     })
 
     it('should return the service when it exists', () => {
-      const a0 = Services.getServiceOrNull(window, 'a')
+      const a0 = Services.getServiceOrNull('a')
       expect(a0).to.be.null
-      Services.registerService(window, 'a', Constructor)
-      const a1 = Services.getService(window, 'a')
-      const a2 = Services.getServiceOrNull(window, 'a')
+      Services.registerService('a', Constructor)
+      const a1 = Services.getService('a')
+      const a2 = Services.getServiceOrNull('a')
       expect(a1).to.equal(a2)
-      const b0 = Services.getServiceOrNull(window, 'b')
+      const b0 = Services.getServiceOrNull('b')
       expect(b0).to.be.null
-      Services.registerService(window, 'b', Constructor)
-      const b1 = Services.getServiceOrNull(window, 'b')
+      Services.registerService('b', Constructor)
+      const b1 = Services.getServiceOrNull('b')
       expect(b1).instanceOf(Constructor)
     })
 
     it('should provide a promise that resolves when instantiated', () => {
-      const p1 = Services.getServicePromise(window, 'a')
-      const p2 = Services.getServicePromise(window, 'a')
+      const p1 = Services.getServicePromise('a')
+      const p2 = Services.getServicePromise('a')
       expect(p1).to.equal(p2)
-      Services.registerService(window, 'a', Constructor)
+      Services.registerService('a', Constructor)
       return Promise.all([p1, p2]).then(([a1, a2]) => {
         expect(a1).to.equal(a2)
         expect(Constructor).to.be.calledOnce
@@ -91,8 +91,8 @@ describe('services', () => {
     })
 
     it('should resolve existing service promise on registering service', () => {
-      const p = Services.getServicePromise(window, 'a')
-      Services.registerService(window, 'a', Constructor)
+      const p = Services.getServicePromise('a')
+      Services.registerService('a', Constructor)
       expect(Constructor).to.be.calledOnce
       return p.then((a) => {
         expect(a).instanceOf(Constructor)
@@ -101,25 +101,25 @@ describe('services', () => {
     })
 
     it('should resolve service promise if service is registered', () => {
-      Services.registerService(window, 'a', Constructor)
+      Services.registerService('a', Constructor)
       expect(Constructor).to.not.be.called
-      return Services.getServicePromise(window, 'a').then((a) => {
+      return Services.getServicePromise('a').then((a) => {
         expect(a).instanceOf(Constructor)
         expect(Constructor).to.be.calledOnce
       })
     })
 
     it('should not return null promise for registered services', () => {
-      Services.registerService(window, 'a', Constructor)
-      const p = Services.getServicePromiseOrNull(window, 'a')
+      Services.registerService('a', Constructor)
+      const p = Services.getServicePromiseOrNull('a')
       expect(p).to.not.be.null
     })
 
     it('should set service builders to null after instantiation', () => {
-      Services.registerService(window, 'a', Constructor)
+      Services.registerService('a', Constructor)
       expect(window.services.a.instance).to.be.null
       expect(window.services.a.Constructor).to.equal(Constructor)
-      Services.getService(window, 'a')
+      Services.getService('a')
       expect(window.services.a.instance).instanceOf(Constructor)
       expect(window.services.a.Constructor).to.be.null
     })
