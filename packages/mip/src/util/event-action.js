154a302fa2c3ae679a3547ae9c090db3d200174f
guozhuorong <Brunoon@mail.ustc.edu.cn>
2019-03-05 21:22:02 +0800

    fix event reg (#530)

--- packages/mip/src/util/event-action.js
+++ packages/mip/src/util/event-action.js
@@ -24,7 +24,7 @@ const PARSE_REG = /^(\w+):\s*([\w-]+)\.([\w-$]+)(?:\((.+)\))?$/
  * @inner
  * @type {RegExp}
  */
-const EVENT_ARG_REG = /^event(\.[a-zA-Z_][\w_]+)+$/g
+const EVENT_ARG_REG = /^event(\.[a-zA-Z_][\w_]*)+$/g
 // const EVENT_ARG_REG_FOR_OBJECT = /(:\s*)(event(\.[a-zA-Z]\w+)+)(\s*[,}])/g
 
 /**
