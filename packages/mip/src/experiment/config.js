fceab30f75a1b298034478660b35aaa951730889
高珊 <39788046+gaoshan003@users.noreply.github.com>
2019-03-27 16:38:43 +0800

    Update config.js

--- packages/mip/src/experiment/config.js
+++ packages/mip/src/experiment/config.js
@@ -9,31 +9,124 @@ export default {
      * 如果有实验需求就按照如下的格式配置
      */
     test1: {
-      description: 'Test1 实验的描述',
+      description: '1 测试开始时间在起始时间和结束时间范围内',
       startTime: '2019-02-13 00:00:00',
       endTime: '2019-04-10 23:59:59',
       sites: [
-        'cp01-sys-rpm14.cp01.baidu.com',
-        'baobao.baidu.com',
-        'muzhi.baidu.com',
-        'm.120ask.com',
         'wenxue.m.iqiyi.com'
+      ]
+    },
+    test2: {
+      description: '2 起始时间默认 结束时间在测试开始时间之后',
+      endTime: '2019-04-10 23:59:59',
+      sites: [
+        'm.120ask.com'
+      ]
+    },
+    test3: {
+      description: '3 起始时间在测试开始时间之前 结束时间默认',
+      startTime: '2019-02-13 00:00:00',
+      sites: [
+        'baobao.baidu.com',
+      ]
+    },
+    test4: {
+      description: '4 起始时间和默认时间都默认',
+      sites: [
+        'muzhi.baidu.com'
+      ]
+    },
+    test5: {
+      description: '5 起始时间在测试开始时间之后',
+      startTime: '2019-03-27 23:59:59',
+      endTime: '2019-04-10 23:59:59',
+      sites: [
+        'm.120ask.com'
+      ]
+    },
+    test6: {
+      description: '6 结束时间在测试开始时间之前',
+      startTime: '2019-03-23 23:59:59',
+      endTime: '2019-03-24 23:59:59',
+      sites: [
+        'm.120ask.com'
+      ]
+    },
+    test7: {
+      description: '7 时间格式错误',
+      startTime: '2019-03-23 23:79:59',
+      endTime: '2019-03-29 23:59:59',
+      sites: [
+        'm.120ask.com'
+      ]
+    },
+    test8: {
+      description: '8 起始时间大于结束时间',
+      startTime: '2019-03-29 23:59:59',
+      endTime: '2019-03-24 23:59:59',
+      sites: [
+        'm.120ask.com'
       ]
     }
   },
 
   abTest: {
     test1: {
-      description: 'abTest1 实验描述',
-      startTime: '2019-03-27 15:30:00',
-      endTime: '2019-03-28 12:08:00',
+      description: '1 测试开始时间在起始时间和结束时间范围内',
+      startTime: '2019-02-13 00:00:00',
+      endTime: '2019-04-10 23:59:59',
       // 所开的流量的百分比
       ratio: 100
     },
     test2: {
-      description: 'abTest2 实验描述',
-      //startTime: '2019-03-21 00:00:00',
-      //endTime: '2019-04-21 23:59:59',
+      description: '2 起始时间默认 结束时间在测试开始时间之后',
+      endTime: '2019-04-10 23:59:59',
+      // 所开的流量的百分比
+      ratio: 100
+    },
+    test3: {
+      description: '3 起始时间在测试开始时间之前 结束时间默认',
+      startTime: '2019-02-13 00:00:00',
+      // 所开的流量的百分比
+      ratio: 100
+    },
+    test4: {
+      description: '4 起始时间和默认时间都默认',
+      // 所开的流量的百分比
+      ratio: 100
+    },
+    test5: {
+      description: '5 起始时间在测试开始时间之后',
+      startTime: '2019-03-27 23:59:59',
+      endTime: '2019-04-10 23:59:59',
+      // 所开的流量的百分比
+      ratio: 100
+    },
+    test6: {
+      description: '6 结束时间在测试开始时间之前',
+      startTime: '2019-03-23 23:59:59',
+      endTime: '2019-03-24 23:59:59',
+      // 所开的流量的百分比
+      ratio: 100
+    },
+    test7: {
+      description: '1-7 测试开始时间在起始时间和结束时间范围内',
+      startTime: '2019-02-13 00:00:00',
+      endTime: '2019-04-10 23:59:59',
+      // 所开的流量的百分比
+      ratio: 0
+    },
+    test8: {
+      description: '1-8 测试开始时间在起始时间和结束时间范围内',
+      startTime: '2019-02-13 00:00:00',
+      endTime: '2019-04-10 23:59:59',
+      // 所开的流量的百分比
+      ratio: 50
+    },
+    test8: {
+      description: '1-8 测试开始时间在起始时间和结束时间范围内',
+      startTime: '2019-02-13 00:00:00',
+      endTime: '2019-04-10 23:59:59',
       // 所开的流量的百分比
       ratio: 10
     }
