9594c090adaa663c92a181f053f021a636a49dcb
tanglei02 <tanglei02@baidu.com>
2019-04-01 13:22:49 +0800

    add log

--- packages/mip/src/util/dom/event.js
+++ packages/mip/src/util/dom/event.js
@@ -103,7 +103,12 @@ function loadPromise (eleOrWindow) {
     }
     // For elements, unlisten on error (don't for Windows).
     if (tagName) {
-      listenOnce(eleOrWindow, 'error', reject)
+      listenOnce(eleOrWindow, 'error', function (e) {
+        if (eleOrWindow.src === 'https://www.wrong.org?test=1') {
+console.log('mip-img error reject')
+        }
+        reject(e)
+      })
     }
   })
 
