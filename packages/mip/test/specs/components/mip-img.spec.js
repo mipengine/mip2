65da87e1910b8292825bc0021bda0d1599e85806
tanglei02 <tanglei02@baidu.com>
2019-04-01 19:13:59 +0800

    change to babel 7.3.4

--- packages/mip/test/specs/components/mip-img.spec.js
+++ packages/mip/test/specs/components/mip-img.spec.js
@@ -12,39 +12,124 @@ import regeneratorRuntime from 'regenerator-runtime'
 /* globals describe, before, it, expect, after, Event */
 
 
-function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {
-  try {
-    var info = gen[key](arg);
-    var value = info.value;
-  } catch (error) {
-    reject(error);
-    return;
-  }
+// function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {
+//   try {
+//     var info = gen[key](arg);
+//     var value = info.value;
+//   } catch (error) {
+//     reject(error);
+//     return;
+//   }
 
-  if (info.done) {
-    resolve(value);
-  } else {
-    Promise.resolve(value).then(_next, _throw);
-  }
-}
+//   if (info.done) {
+//     resolve(value);
+//   } else {
+//     Promise.resolve(value).then(_next, _throw);
+//   }
+// }
 
-function _asyncToGenerator(fn) {
-  return function() {
-    var self = this,
-        args = arguments;
-    return new Promise(function(resolve, reject) {
-      var gen = fn.apply(self, args);
-      function _next(value) {
-        asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value);
-      }
+// function _asyncToGenerator(fn) {
+//   return function() {
+//     var self = this,
+//         args = arguments;
+//     return new Promise(function(resolve, reject) {
+//       var gen = fn.apply(self, args);
+//       function _next(value) {
+//         asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value);
+//       }
 
-      function _throw(err) {
-        asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err);
-      }
-      _next(undefined);
-    });
-  };
-}
+//       function _throw(err) {
+//         asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err);
+//       }
+//       _next(undefined);
+//     });
+//   };
+// }
+
+// function main() {
+//   return _main.apply(this, arguments);
+// }
+
+// function _main() {
+//   _main = _asyncToGenerator(
+//   /*#__PURE__*/
+//   regeneratorRuntime.mark(function _callee2() {
+//     var arr, fn, _fn;
+
+//     return regeneratorRuntime.wrap(function _callee2$(_context2) {
+//       while (1) {
+//         switch (_context2.prev = _context2.next) {
+//           case 0:
+//             _fn = function _ref2() {
+//               _fn = _asyncToGenerator(
+//               /*#__PURE__*/
+//               regeneratorRuntime.mark(function _callee() {
+//                 return regeneratorRuntime.wrap(function _callee$(_context) {
+//                   while (1) {
+//                     switch (_context.prev = _context.next) {
+//                       case 0:
+//                         _context.prev = 0;
+//                         _context.next = 3;
+//                         return new Promise(function (resolve, reject) {
+//                           arr.push(function () {
+//                             console.log('a');
+//                             reject();
+//                           });
+//                         });
+
+//                       case 3:
+//                         _context.next = 8;
+//                         break;
+
+//                       case 5:
+//                         _context.prev = 5;
+//                         _context.t0 = _context["catch"](0);
+//                         console.log('c');
+
+//                       case 8:
+//                       case "end":
+//                         return _context.stop();
+//                     }
+//                   }
+//                 }, _callee, null, [[0, 5]]);
+//               }));
+//               return _fn.apply(this, arguments);
+//             };
+
+//             fn = function _ref() {
+//               return _fn.apply(this, arguments);
+//             };
+
+//             arr = [];
+//             fn();
+//             _context2.next = 6;
+//             return new Promise(function (resolve) {
+//               arr.push(function () {
+//                 console.log('b');
+//                 resolve();
+//               });
+//               arr.forEach(function (item) {
+//                 return item();
+//               });
+//             });
+
+//           case 6:
+//             console.log('d');
+
+//           case 7:
+//           case "end":
+//             return _context2.stop();
+//         }
+//       }
+//     }, _callee2);
+//   }));
+//   return _main.apply(this, arguments);
+// }
+
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
 function main() {
   return _main.apply(this, arguments);
@@ -125,6 +210,8 @@ function _main() {
   }));
   return _main.apply(this, arguments);
 }
+
+
 
 describe('mip-img', function () {
   let mipImgWrapper
