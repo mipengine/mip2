8456da81da2bb808cef1be168e5f3706baa88bfd
tanglei02 <tanglei02@baidu.com>
2019-04-23 13:38:28 +0800

    remove polyfill

--- packages/mip/src/index.js
+++ packages/mip/src/index.js
@@ -5,11 +5,11 @@
 
 /* eslint-disable import/no-webpack-loader-syntax */
 // add polyfills
-import 'script-loader!deps/fetch'
+// import 'script-loader!deps/fetch'
 import 'script-loader!deps/fetch-jsonp'
-import 'script-loader!document-register-element/build/document-register-element'
-import 'deps/promise'
-import 'deps/object-assign'
+// import 'script-loader!document-register-element/build/document-register-element'
+// import 'deps/promise'
+// import 'deps/object-assign'
 /* eslint-enable import/no-webpack-loader-syntax */
 
 import {getRuntime} from './runtime'
