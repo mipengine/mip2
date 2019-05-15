325da8727961c377f80010926b08bc7735fc61d7
guozhuorong <guozhuorong@baidu.com>
2019-05-15 13:43:49 +0800

    fix ut

--- packages/mip/test/specs/log/firstsceen-label.spec.js
+++ packages/mip/test/specs/log/firstsceen-label.spec.js
@@ -22,22 +22,20 @@ describe('firstscreen-label', () => {
     `
     document.body.appendChild(div)
   })
-  
+
   it('correct firstsceen label log', function (done) {
     let spy = sinon.stub(viewer, 'sendMessage').callsFake(function (eventName, data = {}) {
       expect(eventName).to.be.equal('performance-analysis-log')
       expect(data).to.be.a('object')
       expect(data.type).to.be.equal('fslabel')
-      expect(data.info).to.be.equal('html.1/body.1/div.12/div.2/mip-img.1!!html.1/body.1/div.12/div.1/mip-img.1')
+      expect(data.info).to.be.equal('html.1/body.1/div.10/div.2/mip-img.1!!html.1/body.1/div.10/div.1/mip-img.1')
       done()
       spy.restore()
     })
     firstSceenLabel.sendLog()
   })
-  
+
   after(() => {
     document.body.removeChild(div)
   })
 })
-
-
