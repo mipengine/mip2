2f1f1af552c7215fd4299a350437181bdd92bcf9
panwenshuang <panwenshuang@baidu.com>
2019-06-11 16:53:35 +0800

    add index.d.ts for compling mip2-extionsions wrote in TypeScript

---
+++ packages/mip-cli/lib/builder/webpack/config/index.d.ts
@@ -0,0 +1,7 @@
+/**
+ * @file add MIP description for TypeScript used in mip2-extensions
+ * @author MiyaPan (270687594@qq.com)
+ * @description MIP 对象的声明文件，用于编译 TypeScript 编写的组件时，识别 MIP 对象，由于核心未使用 TypeScript 定义类型，暂时定义为 any
+ */
+
+declare const MIP: any
